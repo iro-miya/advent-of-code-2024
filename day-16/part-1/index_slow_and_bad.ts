e9f3a8a1b2bc878c0872340f37c886c229efe9b3
Miya Ironami <miya@ironami.org>
2024-12-19 17:35:20 +0100

    Day 16

---
+++ day-16/part-1/index_slow_and_bad.ts
@@ -0,0 +1,224 @@
+import { runner } from "../../runner.ts";
+
+interface Coords {
+	x: number;
+	y: number;
+}
+
+enum Tile {
+	Empty,
+	Wall,
+}
+
+enum Direction {
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
+function move({ x, y }: Coords, direction: Direction, steps: number = 1): Coords {
+	switch (direction) {
+		case Direction.Up:
+			return { x, y: y - steps };
+
+		case Direction.Right:
+			return { x: x + steps, y };
+
+		case Direction.Down:
+			return { x, y: y + steps };
+
+		case Direction.Left:
+			return { x: x - steps, y };
+	}
+}
+
+interface ReindeerOptions {
+	level: Tile[][];
+	position?: Coords;
+	visited?: Coords[];
+	direction: Direction;
+	score?: number | null | undefined;
+}
+
+class Reindeer {
+	visited: Coords[] = [];
+	level: Tile[][];
+	direction: Direction;
+	score: number = 0;
+
+	constructor({ level, position, visited, direction, score }: ReindeerOptions) {
+		this.level = level;
+
+		if (visited) {
+			this.visited = visited;
+		}
+
+		if (position) {
+			this.visited.push(position);
+		}
+
+		this.direction = direction;
+
+		if (score != null) {
+			this.score = score;
+		}
+	}
+
+	get position(): Coords {
+		return Object.freeze(this.visited[this.visited.length - 1]);
+	}
+
+	possibleDirections(): Set<Direction> {
+		const directions: [Direction, Coords][] = [
+			[Direction.Up, move(this.position, Direction.Up)],
+			[Direction.Right, move(this.position, Direction.Right)],
+			[Direction.Down, move(this.position, Direction.Down)],
+			[Direction.Left, move(this.position, Direction.Left)],
+		];
+
+		const possible = new Set<Direction>();
+
+		for (const [direction, position] of directions) {
+			if (
+				this.level[position.y][position.x] === Tile.Empty
+				&& !this.visited.some(
+					(visited) => visited.x === position.x && visited.y === position.y
+				)
+			) {
+				possible.add(direction);
+			}
+		}
+
+		return possible;
+	}
+
+
+	*advance(): Generator<Reindeer> {
+		const newPos = move(this.position, this.direction);
+
+		if (this.level[newPos.y][newPos.x] === Tile.Empty) {
+			this.visited.push(newPos);
+			this.score++;
+
+			for (const direction of this.possibleDirections()) {
+				if (direction === this.direction) {
+					yield this;
+				} else {
+					yield new Reindeer({
+						level: this.level,
+						visited: [...this.visited],
+						direction,
+						score: this.score + 1_000,
+					});
+				}
+			}
+		}
+	}
+
+	distanceTo(point: Coords) {
+		return Math.sqrt(
+			Math.abs(this.position.x - point.x) ** 2
+			+ Math.abs(this.position.y - point.y) ** 2,
+		)
+	}
+}
+
+function reduceLowest<T>(getter: (elem: T) => number) {
+	return (acc: T[], elem: T): T[] => {
+		if (acc.length === 0) {
+			return [elem];
+		}
+
+		const currVal = getter(acc[0]);
+		const newVal = getter(elem);
+
+		if (newVal < currVal) {
+			return [elem];
+		}
+
+		if (newVal === currVal) {
+			acc.push(elem);
+		}
+
+		return acc;
+	}
+}
+
+
+function solve(input: string): number {
+	const start: Coords = {
+		x: 0,
+		y: 0,
+	};
+
+	const end: Coords = {
+		x: 0,
+		y: 0,
+	};
+
+	const level: Tile[][] = input.split(/\r?\n/g).map((line, y) =>
+		Iterator.from(line).map((char, x) => {
+			if (char === "#") {
+				return Tile.Wall;
+			}
+
+			if (char === "S") {
+				start.x = x;
+				start.y = y;
+				return Tile.Empty;
+			}
+
+			if (char === "E") {
+				end.x = x;
+				end.y = y;
+				return Tile.Empty;
+			}
+
+			if (char === ".") {
+				return Tile.Empty;
+			}
+
+			throw new Error("Unknown character: " + char);
+		}).toArray(),
+	);
+
+	const reindeers: Set<Reindeer> = new Set([
+		new Reindeer({ level, position: start, direction: Direction.Up }),
+		new Reindeer({ level, position: start, direction: Direction.Right }),
+		new Reindeer({ level, position: start, direction: Direction.Down }),
+		new Reindeer({ level, position: start, direction: Direction.Left }),
+	]);
+
+	while (reindeers.size > 0) {
+		const bestReindeers = Iterator.from(reindeers)
+			.reduce(reduceLowest(reindeer => reindeer.score), [])
+			.reduce(reduceLowest(reindeer => reindeer.distanceTo(end)), []);
+
+		const reindeer = bestReindeers[0];
+
+		reindeers.delete(reindeer);
+
+		for (const otherReindeer of reindeers) {
+			if (
+				reindeer.position.x === otherReindeer.position.x
+				&& reindeer.position.y === otherReindeer.position.y
+				&& reindeer.direction === otherReindeer.direction
+			) {
+				reindeers.delete(otherReindeer);
+			}
+		}
+
+		for (const newReindeer of reindeer.advance()) {
+			if (newReindeer.position.x === end.x && newReindeer.position.y === end.y) {
+				return newReindeer.score + 1;
+			}
+
+			reindeers.add(newReindeer);
+		}
+	}
+
+	throw new Error("No solution found");
+}
+
+await runner(solve);
---
+++ day-16/part-2/index_fast_and_good.ts
@@ -0,0 +1,460 @@
+import { runner } from "../../runner.ts";
+
+enum Direction {
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
+// A graph connection from one node to another
+interface Connection {
+	start: number;
+	end: number;
+
+	// The references of every unique point crossed
+	// This allows us to know how many are crossed at the end
+	points: Set<number>,
+	score: number;
+
+	// The direction allows us to add the 1000 points penalty on turning.
+	startDirection: Direction;
+	// Connections are simplified and only intersections are kept.
+	// Because of that, a connection may end in a different direction than it started in.
+	endDirection: Direction;
+}
+
+// Iterates an array from top to bottom and then from left to right
+// Generates a generator for every row, so you can iterate with two nested for loops
+function * iterateVertically<T>(arr: T[][]): Generator<Generator<T>> {
+	const height = arr.length;
+	const width = arr[0]?.length ?? 0;
+
+	function * iterateRow(x: number) {
+		for (let y = 0; y < height; y++) {
+			yield arr[y][x];
+		}
+	}
+
+	for (let x = 0; x < width; x++) {
+		yield iterateRow(x);
+	}
+}
+
+// Deletes all dead-ends in a graph, except the ones in the allowed set.
+// Also deletes all nodes that link to nothing.
+// Returns a boolean indicating whether any changes were made
+function deleteDeadEnds(connectionMap: Map<number, Set<Connection>>, allowed?: Set<number>): boolean {
+	let changed = false;
+	const toCheck = new Set(connectionMap.keys());
+
+	// while loop because elements may be added during iteration
+	while (toCheck.size > 0) {
+		const ref = toCheck.values().next().value!;
+		toCheck.delete(ref);
+
+		if (allowed && allowed.has(ref)) {
+			continue;
+		}
+
+		const connections = connectionMap.get(ref)!;
+
+		if (connections.size <= 1) {
+			// Only one connection = dead-end
+			connectionMap.delete(ref);
+			changed = true;
+
+			// Delete the connection from the node that links to this one
+
+			const [connection] = connections; // There is only a single connection
+
+			if (connection) {
+				const inward = connectionMap.get(connection.end)!;
+				const deadLinks = inward.values()
+					.filter(val => val.end === ref);
+
+				for (const deadLink of deadLinks) {
+					inward.delete(deadLink);
+				}
+
+				// Check whether we didn't create a dead-end just now
+				toCheck.add(connection.end);
+			}
+		}
+	}
+
+	return changed;
+}
+
+// Collapses paths that aren't intersections, removing the node.
+// Those nodes are not interesting since they don't offer any choice.
+// Returns a boolean indicating whether any changes were made
+function collapseStraightPaths(connectionMap: Map<number, Set<Connection>>, allowed: Set<number>): boolean {
+	let changed = false;
+
+	for (const [ref, connections] of connectionMap) {
+		if (connections.size === 2 && !allowed.has(ref)) {
+			// This is a straight node, it needs to be removed.
+
+			// Get both outward connections
+			const [con1, con2] = connections;
+			
+			// Collapse them both ways
+			for (const [outward, end] of [[con1, con2], [con2, con1]]) {
+				const startConnections = connectionMap.get(outward.end)!;
+
+				for (const start of startConnections) {
+					// start is the connection going to this node
+					// end is the connection goint out of this node
+					// Since the node is getting removed, they get collapsed.
+					if (start.end === ref) {
+						start.end = end.end;
+						start.score += end.score;
+						start.points = start.points.union(end.points);
+
+						if (start.endDirection !== end.startDirection) {
+							start.score += 1000;
+						}
+
+						start.endDirection = end.endDirection;
+					}
+				}
+			}
+
+			connectionMap.delete(ref);
+			changed = true;
+		}
+	}
+
+	return changed;
+}
+
+// Deletes connections when another connection with the same properties but a lower cost exists.
+// Also deletes connections that lead to the same node.
+// Returns a boolean indicating whether any changes were made
+function deleteUselessPaths(connectionMap: Map<number, Set<Connection>>): boolean {
+	let changed = false;
+
+	for (const [ref, connections] of connectionMap) {
+		for (const connection of connections) {
+			if (connection.start === connection.end
+				|| connections.values().some(
+					(val) => val !== connection
+						&& val.start === connection.start
+						&& val.end === connection.end
+						&& val.startDirection === connection.startDirection
+						&& val.endDirection === connection.endDirection
+						&& val.score < connection.score
+				)
+			) {
+				connections.delete(connection);
+				changed = true;
+
+				if (connections.size === 0) {
+					connectionMap.delete(ref);
+				}
+			}
+		}
+	}
+
+	return changed;
+}
+
+// Reduce function that runs a map function on every element,
+// and produces an array of the elements for which the map function produced the lowest values.
+// The call to .reduce must have an empty array as the second argument.
+function reduceLowest<T>(getter: (elem: T) => number) {
+	return (acc: T[], elem: T): T[] => {
+		if (acc.length === 0) {
+			return [elem];
+		}
+
+		const currVal = getter(acc[0]);
+		const newVal = getter(elem);
+
+		if (newVal < currVal) {
+			return [elem];
+		}
+
+		if (newVal === currVal) {
+			acc.push(elem);
+		}
+
+		return acc;
+	}
+}
+
+function solve(input: string) {
+	// The graph representing every possible path in the labyrinth
+	const connectionMap: Map<number, Set<Connection>> = new Map();
+
+	const level = input.split(/\r?\n/g).map(
+		line => [...line],
+	);
+
+	const height = level.length;
+	const width = level[0]?.length ?? 0;
+
+	let start: number = 0;
+	let end: number = 0;
+
+	// For performance, coordinates as saved as a single integer
+	function pointToRef(x: number, y: number) {
+		return (y * width) + x;
+	}
+
+	function refToPoint(ref: number): [number, number] {
+		return [ref % width, Math.floor(ref / width)];
+	}
+
+	// We iterate the grid in both directions to record every path as a graph
+	for (const [i, grid] of [level, iterateVertically(level)].entries()) {
+		let direction: Direction; // Parsing direction
+		let mirrorDirection: Direction; // Reverse direction
+
+		if (i === 0) {
+			direction = Direction.Right;
+			mirrorDirection = Direction.Left;
+		} else {
+			direction = Direction.Down;
+			mirrorDirection = Direction.Up;
+		}
+
+		// grid and line may be an array or a generator
+		// forEach works on both arrays and generators
+		grid.forEach((line, index1) => {
+			// When we find multiple empty spaces in a row, we create a connection
+			let currentConnection: Connection | null = null;
+
+			line.forEach((char, index2) => {
+				let x: number;
+				let y: number;
+
+				// Get adjacent characters so we can check for intersections
+				let charAbove: string;
+				let charBelow: string;
+
+				if (direction === Direction.Right) {
+					x = index2;
+					y = index1;
+					charAbove = level[y - 1]?.[x];
+					charBelow = level[y + 1]?.[x];
+					
+				} else {
+					x = index1;
+					y = index2;
+					charAbove = level[y][x - 1];
+					charBelow = level[y][x + 1];
+				}
+
+				// Empty space: add point to current connection
+				if (char !== "#" && currentConnection != null) {
+					currentConnection.end = pointToRef(x, y);
+					currentConnection.points.add(pointToRef(x, y));
+					currentConnection.score++;
+				}
+
+				// Wall or intersection: Terminate current connection
+				if (
+					currentConnection != null && (
+						char === "#"
+						|| charAbove !== "#"
+						|| charBelow !== "#"
+					)
+				) {
+					if (currentConnection.score > 0) {
+						// Create a "mirror" connection because nodes must link both ways
+						const mirror: Connection = {
+							start: currentConnection.end,
+							end: currentConnection.start,
+							startDirection: mirrorDirection,
+							endDirection: mirrorDirection,
+							points: new Set(currentConnection.points),
+							score: currentConnection.score,
+						}
+
+						for (const connection of [currentConnection, mirror]) {
+							// Initialise an empty set if needed
+							if (connectionMap.has(connection.start)) {
+								connectionMap.get(connection.start)!.add(connection);
+							} else {
+								connectionMap.set(connection.start, new Set<Connection>().add(connection));
+							}
+						}
+					}
+
+					currentConnection = null;
+				}
+
+				// Empty spaces: Start new connection if there isn't one
+				// Notice that in the case of an intersection or an S/E character, all three conditions will run.
+				if (char !== "#" && currentConnection == null) {
+					currentConnection = {
+						start: pointToRef(x, y),
+						end: pointToRef(x, y),
+						points: new Set<number>().add(pointToRef(x, y)),
+						score: 0,
+						startDirection: direction,
+						endDirection: direction,
+					}
+				}
+
+				// On the first iteration, record the position of S and E
+				if (direction === Direction.Right) {
+					if (char === "S") {
+						start = pointToRef(x, y);
+					} else if (char === "E") {
+						end = pointToRef(x, y);
+					}
+				}
+			});
+		});
+	}
+
+	// Graph simplification: the simplification algorithms are run until the graph is stable (it stops changing)
+	while (true) {
+		const deadEnds = deleteDeadEnds(connectionMap, new Set([start, end]));
+		const nodesCollapsed = collapseStraightPaths(connectionMap, new Set([start, end]));
+		const uselessPaths = deleteUselessPaths(connectionMap);
+
+		if (!deadEnds && !nodesCollapsed && !uselessPaths) {
+			break;
+		}
+	}
+
+	// Turn the graph into an SVG to check how it looks
+	/*
+	let svg = `<svg version="2" width="${width * 10}" height="${height * 10}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">`
+
+	for (const [ref, connections] of connectionMap) {
+		const [startX, startY] = refToPoint(ref);
+
+		svg += `<circle cx="${startX}" cy="${startY}" r="1" fill="red" />`;
+
+		for (const connection of connections) {
+			const [endX, endY] = refToPoint(connection.end);
+			svg += `<line x1="${startX}" y1="${startY}" x2="${endX}" y2="${endY}" stroke="black" />`
+		}
+	}
+
+	const [startX, startY] = refToPoint(start);
+	svg += `<circle cx="${startX}" cy="${startY}" r="1" fill="green" />`;
+
+	const [endX, endY] = refToPoint(end);
+	svg += `<circle cx="${endX}" cy="${endY}" r="1" fill="blue" />`;
+	svg += "</svg>";
+
+	return svg;
+	//*/
+
+	// Pathfinding!!!
+
+	interface Attempt {
+		history: Set<Connection>,
+		visited: Set<number>,
+		location: number,
+		score: number;
+		direction: Direction | null;
+	}
+
+	// The current attempts
+	// On every iteration, we pick one the one with the lowest score and expand it in every possible direction
+	const attempts: Set<Attempt> = new Set<Attempt>().add(
+		{
+			history: new Set(),
+			visited: new Set<number>().add(start),
+			location: start,
+			score: 0,
+			direction: null,
+		}
+	);
+
+	// The lowest scores in which each connection was traversed.
+	// It's useless to attempt to traverse a connection if it would result in a higher score than a previous attempt,
+	// so we skip those cases.
+	const highScores: Map<Connection, number> = new Map();
+
+	let winningScore = Infinity;
+	const winners: Set<Attempt> = new Set();
+
+	while (attempts.size > 0) {
+		// Find the attempt with the lowest score (or one of them)
+		const [bestAttempt] = attempts.values()
+			.reduce(reduceLowest((attempt) => attempt.score), []);
+
+		attempts.delete(bestAttempt);
+
+		// If the best attempt has a higher score than the lowest winning score,
+		// then we found every lowest winning paths.
+		if (bestAttempt.score > winningScore) {
+			break;
+		}
+
+		// If the attempt turns out to have reached the end, then we found a winner
+		if (bestAttempt.location === end) {
+			if (bestAttempt.score < winningScore) {
+				// If it improves on the lowest winning score, then we clear the current winners
+				winningScore = bestAttempt.score;
+				winners.clear();
+			}
+
+			winners.add(bestAttempt);
+			continue;
+		}
+
+		// Get every connection from our current location and expand in all of them.
+		const connections = connectionMap.get(bestAttempt.location)!;
+
+		for (const connection of connections) {
+			if (bestAttempt.visited.has(connection.end)) {
+				// Won't go back to a previously visited location.
+				continue;
+			}
+
+			const highScore = highScores.get(connection) ?? Infinity;
+			let newScore = bestAttempt.score + connection.score;
+
+			if (bestAttempt != null && bestAttempt.direction !== connection.startDirection) {
+				newScore += 1000;
+			}
+
+			// Don't go through a connection if we already managed to visit it with a lower score
+			// Since we must find every winning path, we keep the paths with equal score
+			if (newScore > highScore) {
+				continue;
+			}
+
+			attempts.add({
+				history: new Set(bestAttempt.history).add(connection),
+				visited: new Set(bestAttempt.visited).add(connection.end),
+				location: connection.end,
+				direction: connection.endDirection,
+				score: newScore,
+			});
+
+			highScores.set(connection, newScore);
+		}
+	}
+
+	// Counts the number of unique points visited
+	let visitedPoints: Set<number> = new Set();
+
+	for (const winner of winners) {
+		for (const connection of winner.history) {
+			visitedPoints = visitedPoints.union(connection.points);
+		}
+	}
+
+	return visitedPoints.size;
+
+	// Visualise the solutions
+	/*
+	for (const [x, y] of visitedPoints.values().map(refToPoint)) {
+		level[y][x] = "O";
+	}
+
+	return level.map(line => line.join("")).join("\n");
+	//*/
+}
+
+await runner(solve);
